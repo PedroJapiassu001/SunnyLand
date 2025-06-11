2c9059525b758eb095bcef0268e1863fbae01401
Pedro Japiassu <pedrojcs2008@gmail.com>
2025-06-11 09:17:14 -0300

    Aulaunity1106

---
+++ SunnyLand/Assets/Scripts/Personagem.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour
+{
+    public int vida = 6;
+    private Rigidbody2D rb;
+    public float novaGravidade = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = novaGravidade;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
