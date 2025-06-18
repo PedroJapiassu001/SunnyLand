902b04ae7f65da09df365d68e2a3765270d61ec6
Pedro Japiassu <pedrojcs2008@gmail.com>
2025-06-18 09:21:27 -0300

    Dia1806

--- SunnyLand/Assets/Scripts/Coletavel.cs
+++ SunnyLand/Assets/Scripts/Coletavel.cs
@@ -1,19 +1,22 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Coletavel : MonoBehaviour
 {
-    private int pontos = 5;
-    // Start is called before the first frame update
-    void Start()
+   public TMP_Text texto_pontos;
+    int pontos = 0;
+    public ParticleSystem efeito;
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        Debug.Log("O jogo começou com" +  pontos  + "pontos");   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        //Debug.Log("Rodando");
+        if (collision.CompareTag("Player"))
+        {
+            pontos++;
+            texto_pontos.text = "Pontos:" + pontos.ToString();
+            collision.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
+            Instantiate(efeito, transform.position, Quaternion.identity);
+        }
     }
 }
